d940f437da2d412625ace040013e52dc20592a09
Ali Elshorpagi <ali.elshorpagi11@gmail.com>
2023-01-31 17:29:37 +0200

    inilized

---
+++ include.cpp
@@ -0,0 +1,10 @@
+#include "source/BinaryTree.cpp"
+// #include "source/Circular_Queue.cpp" // this file was compiled in another file
+#include "source/Deque.cpp"
+#include "source/Doubly_Linkedlist.cpp"
+#include "source/Priority_Queue.cpp"
+#include "source/Queue_Linkedlist_Based.cpp"
+// #include "source/Singly_Linkedlist.cpp" // this file was compiled in another file
+#include "source/Stack_Array_Based.cpp"
+#include "source/Stack_Linkedlist_Based.cpp"
+#include "source/Vector.cpp"
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include "include.cpp"
+
+using namespace std;
+
+typedef long long ll;
+
+#define _CRT_SECURE_NO_DEPRECATE
+#define Mesh_Ali (ios_base::sync_with_stdio(false), cin.tie(NULL))
+#define edl '\n'
+
+int main()
+{
+	Mesh_Ali;
+    
+	cout << edl << "DONE" << edl;
+	return 0;
+}
