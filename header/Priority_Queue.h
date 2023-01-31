340a7c99866ee3dec0ebfe56e49558adf40ddb7d
Ali Elshorpagi <ali.elshorpagi11@gmail.com>
2023-01-31 19:03:41 +0200

    Priority_Queue

---
+++ header/Priority_Queue.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Circular_Queue.h"
+#include "../source/Circular_Queue.cpp"
+
+template <class type>
+class Priority_Queue
+{
+    ll size{};
+    ll added_elements{};
+    Circular_Queue<type> q1, q2, q3;
+
+public:
+    Priority_Queue(ll sz);
+    ~Priority_Queue();
+};
---
+++ source/Priority_Queue.cpp
@@ -0,0 +1,11 @@
+#include "..\\header\Priority_Queue.h"
+
+template <class type>
+Priority_Queue<type>::Priority_Queue(ll sz) : size(sz), q1(sz), q2(sz), q3(sz)
+{
+}
+
+template <class type>
+Priority_Queue<type>::~Priority_Queue()
+{
+}
