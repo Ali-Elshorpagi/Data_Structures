0de7229ad550ff3721fdec5a2ac8de6c69c3c58c
Ali Elshorpagi <ali.elshorpagi11@gmail.com>
2023-02-27 13:51:53 +0200

    modified:

--- main.cpp
+++ main.cpp
@@ -11,7 +11,10 @@ int main()
 	Mesh_Ali;
 	// freopen("test/input.txt", "r", stdin);
 	freopen("test/output.txt", "w", stdout);
-
+	Vector<int> vec(4);
+	for (int i(0); i < 4; ++i)
+		cin >> vec[i];
+	vec.print();
 	cout << edl << "DONE" << edl;
 	return (0);
 }
