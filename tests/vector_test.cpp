246dc5281d0483fb3ef04d37172e067a302a366c
Ali-Elshorpagi <ali.elshorpagi11@gmail.com>
2023-07-26 05:54:40 +0300

    add vector test

--- tests/vector_test.cpp
+++ tests/vector_test.cpp
@@ -1,8 +1,5 @@
 #include "../source/Vector.cpp"
 
-using namespace std;
-#define edl '\n'
-
 const void test_get_size()
 {
     Vector<int> tester(3);
@@ -48,35 +45,113 @@ const void test_get_back()
     assert(tester.get_back() == 12);
 }
 
-const void test_push_back(){}
-// const void test_pop_back();
-// const void test_pop();
-// const void test_remove();
-// const void test_set_value();
-// const void test_find();
-// const void test_insert();
-// const void test_right_rotate();
-// const void test_left_rotate();
-// const void test_find_transposition();
+const void test_pop_back()
+{
+    Vector<int> tester(1);
+    tester.push_back(1);
+    tester.push_back(2);
+    tester.push_back(3);
+
+    tester.pop_back();
+    assert(tester.get_back() == 2);
+    tester.push_back(3);
+    assert(tester.get_size() == 4);
+
+    tester.pop_back(), tester.pop_back();
+    assert(tester.get_back() == 1);
+    assert(tester.get_size() == 2);
+}
+
+const void test_pop()
+{
+    Vector<int> tester;
+    tester.push_back(1);
+    tester.push_back(2);
+    tester.push_back(3);
+    tester.push_back(3);
+
+    int popped(tester.pop(0));
+    assert(popped == 1);
+    tester.push_back(9);
+    tester.push_back(8);
+
+    assert(tester.get_size() == 5);
+    int popped2(tester.pop(4));
+    assert(popped2 == 8);
+    assert(tester.get_size() == 4);
+}
+
+const void test_remove()
+{
+    Vector<int> tester;
+    tester.push_back(-1);
+    tester.push_back(2);
+    tester.push_back(-1);
+    tester.push_back(4);
+    tester.push_back(-1);
+    tester.remove(-1);
+    assert(tester.get_size() == 2);
+    assert(tester.get_value(0) == 2);
+    assert(tester.get_value(1) == 4);
+}
+
+const void test_set_value()
+{
+    Vector<int> tester;
+    tester.push_back(5);
+    tester.push_back(7);
+    tester.push_back(9);
+    tester.set_value(0, 4);
+    assert(tester.get_value(0) == 4);
+    assert(tester.get_value(1) == 7);
+    assert(tester.get_value(2) == 9);
+    tester.set_value(2, 8);
+    assert(tester.get_value(2) == 8);
+}
+
+const void test_find()
+{
+    Vector<int> tester;
+    tester.push_back(4);
+    tester.push_back(7);
+    tester.push_back(-5);
+    assert(tester.find(5) == -1);
+    assert(tester.find(4) == 0);
+    assert(tester.find(7) == 1);
+    assert(tester.find(-5) == 2);
+}
+
+const void test_insert()
+{
+    Vector<int> tester;
+    tester.push_back(5);
+    tester.push_back(7);
+    tester.push_back(9);
+    tester.insert(0, 4);
+    assert(tester.get_value(0) == 4);
+    assert(tester.get_value(1) == 5);
+    assert(tester.get_value(2) == 7);
+    assert(tester.get_value(3) == 9);
+    tester.insert(3, 8);
+    assert(tester.get_value(3) == 8);
+    assert(tester.get_value(4) == 9);
+}
 
 int main()
 {
     freopen("../imports/test/output.txt", "w", stdout);
-    // test_get_size();
-    // test_is_empty();
-    // test_get_value();
-    // test_get_front();
-    // test_get_back();
-    test_push_back();
-    // test_pop_back();
-    // test_pop();
-    // test_remove();
-    // test_set_value();
-    // test_find();
-    // test_insert();
-    // test_right_rotate();
-    // test_left_rotate();
-    // test_find_transposition();
+    test_get_size();
+    test_is_empty();
+    test_get_value();
+    test_get_front();
+    test_get_back();
+    test_pop_back();
+    test_pop();
+    test_remove();
+    test_set_value();
+    test_find();
+    test_insert();
     cout << edl << "DONE" << edl;
+    // you must see 'DONE' in output
     return (0);
 }
