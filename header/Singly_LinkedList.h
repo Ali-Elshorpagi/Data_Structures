c14c81a6f45333ac05b24b3dfd0b744300532b9e
Ali-Elshorpagi <ali.elshorpagi11@gmail.com>
2023-09-11 00:02:09 +0300

    initials descriotoin for some functions in Singlylinkedlist

--- header/Singly_LinkedList.h
+++ header/Singly_LinkedList.h
@@ -41,39 +41,68 @@ public:
 
     Singly_LinkedList();
     ~Singly_LinkedList();
+    // Returns the value at the given index.
     const type get_value(int idx);
+    // Returns the length (number of nodes) of the linked list.
     const int get_length();
+    // Inserts a new node with the given value at the end of the linked list.
     void insert_end(type val);
+    // Inserts a new node with the given value at the beginning of the linked list.
     void insert_front(type val);
+    // Inserts a new node with the given value after a specific node (`src`) in the linked list.
     void insert_after(S_Node<type> *src, S_Node<type> *target);
+    // Inserts a new node with the given value into the list while maintaining sorted order.
     void insert_sorted(type val);
+    // Prints the values of all nodes in the linked list
     void print();
+    // Recursively prints the values of nodes in the linked list
     void print_recr(S_Node<type> *h);
+    // Recursively prints the values of nodes in reverse order
     void print_recr_rever(S_Node<type> *h);
+    // Returns the index of the node containing the value, or -1 if not found.
     int search(type val);
     int search_improved(type val);
     int search_improved_v2(type val);
+    // Returns `true` if the lists are the same, otherwise `false`.
     bool is_same(const Singly_LinkedList<type> &list);
+    // Removes and returns the value of the node at the front of the linked list.
     type pop_front();
+    // Removes and returns the value of the node at the end of the linked list.
     type pop_back();
+    // Deletes the node at the specified index `n` in the linked list.
     void delete_nth_node(int n);
+    // Deletes the node that is the next ot the given node `node` in the linked list.
     void delete_next_node(S_Node<type> *node);
+    // Deletes the first occurrence of the node containing the specified value from the linked list.
     void delete_node_with_key(type val);
+    // Deletes nodes at even positions (0-based index) from the linked list.
     void delete_even_positions();
+    // Deletes the last occurrence of the specified value from the linked list.
     void delete_last_occurrence_not_sorted(type key);
+    // Removes duplicate values from the linked list, which is not necessarily sorted.
     void remove_duplicates_not_sorted();
+    // Swaps adjacent elements in the linked list, effectively repairing it.
     void swap_repairs();
+    // Reverses the order of nodes in the linked list.
     void reverse();
     void swap_head_tail();
     void left_rotate(int key);
     void move_key_occurance_to_end_not_sorted(type key);
+    // Returns the maximum value present in the linked list.
     type max_value();
+    // Returns the minimum value present in the linked list.
     type min_value();
+    // Returns the middle value of the linked list.
     type middle_value();
+    // Rearranges nodes in such a way that odd-positioned nodes appear before even-positioned nodes.
     void arrange_odd_pos_even_pos();
+    // Inserts nodes from another linked list at alternate positions in the current linked list.
     void insert_alternate(Singly_LinkedList<type> &other);
+    // Merges two sorted linked lists iteratively into a single sorted linked list.
     S_Node<type> *merge_two_lists_iterative(S_Node<type> &head1, S_Node<type> &head2);
+    // Merges two sorted linked lists recursively into a single sorted linked list.
     S_Node<type> *merge_two_lists_recursive(S_Node<type> &head1, S_Node<type> &head2);
+    // Checks if the linked list is a palindrome (reads the same forwards and backwards).
     bool is_palindrome();
 };
 #endif
